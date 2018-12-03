810e2f783c442ec4c5dff64419cc059dc184e096
Pascal Kiser <pascal.kiser@ajila.com>
2018-12-03 19:57:27 +0100

    Implemented logo spinner button

--- brapple/brapple/LogoViewController.swift
+++ brapple/brapple/LogoViewController.swift
@@ -13,6 +13,9 @@ class LogoViewController: UIViewController {
     // TODO: JSON-Rezept erstellen nach folgender Vorlage:
     // https://craftbeer-revolution.de/lexikon/bier-selber-brauen/india-pale-rezept
     // Datentypen: siehe RecipeViewController
+    
+    @IBOutlet weak var logo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +25,10 @@ class LogoViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func spinnerButton(_ sender: UIButton) {
+        logo.transform = logo.transform.rotated(by: CGFloat(Double.pi / 4))
+    }
+
     // TODO: fake loading bar and/or splash screen effect
 }
 
